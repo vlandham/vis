70ff971c2b3daee0b05d65131e94a0fc745f3dc3
Jim V <jfv@stowers.org>
2011-12-30 13:19:19 -0600

    add download svg code

--- view/js/script.js
+++ view/js/script.js
@@ -1,6 +1,34 @@
-/* Author: 
+$(document).ready(function() {
+  $("#generate").click(writeDownloadLink);
+});
 
-*/
+function writeDownloadLink(){
+    var html = d3.select("svg")
+        .attr("title", "test2")
+        .attr("version", 1.1)
+        .attr("xmlns", "http://www.w3.org/2000/svg")
+        .node().parentNode.innerHTML;
+
+    d3.select("#preview").append("div")
+        .attr("id", "download")
+        .style("top", event.clientY+20+"px")
+        .style("left", event.clientX+"px")
+        .html("Right-click on this preview and choose Save as<br />Left-Click to dismiss<br />")
+        .append("img")
+        .attr("src", "data:image/svg+xml;base64,"+ btoa(html));
+
+    d3.select("#download")
+        .on("click", function(){
+            if(event.button == 0){
+                d3.select(this).transition()
+                    .style("opacity", 0)
+                    .remove();
+            }
+        })
+        .transition()
+        .duration(500)
+        .style("opacity", 1);
+};
 
 
 
