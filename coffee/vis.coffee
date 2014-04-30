6444ccd60b6dac5d77ef115e50207c7e099a9815
Jim Vallandingham <vlandham@gmail.com>
2014-04-30 15:39:14 -0700

    real parsing

--- coffee/vis.coffee
+++ coffee/vis.coffee
@@ -3,19 +3,31 @@ root = exports ? this
 
 parsePath = (path) ->
   cmdRegEx = /[a-z][^a-z]*/ig
+  whitespaceRegEx = /\s/g
+  path = path.replace(whitespaceRegEx, "")
   sections = path.match(cmdRegEx)
   parsed = []
   sections.forEach (raw) ->
     command = raw.slice(0,1)
-    nums = raw.slice(1).split(",").map((d) -> parseFloat(d)).filter((d) -> !isNaN(d))
+    nums = raw.slice(1).split(",").map (d) ->
+      (d)
+    # merged = []
+    # merged = merged.concat.apply(merged, nums)
+    # nums = nums.filter((d) -> !isNaN(d))
     parsedCommand = {'c':command}
     # if nums.length > 1
     parsedCommand['n'] = nums
     parsed.push(parsedCommand)
   parsed
 
+joinPath = (parsed) ->
+  path = ""
+  parsed.forEach (p) ->
+    c = p.c + p.n.join(",")
+    path = path.concat(c)
+  path
 
-
+  
 $ ->
   d3.xml 'data/bike.svg', 'image/svg+xml', (error, data) ->
     console.log(error)
@@ -32,8 +44,16 @@ $ ->
       .selectAll("path")
       .attr("fill", "steelblue")
       .each (d) ->
-        path = d3.select(this).attr("d")
-        console.log(parsePath(path))
+        t = d3.select(this)
+        path = t.attr("d")
+        pp = (parse(path))
+        t.attr("d", join(pp))
+        
+
+        console.log(path)
+        console.log(join(pp))
+        console.log("---")
+
       
 
 
---
+++ js/parse.js
@@ -0,0 +1,57 @@
+var length = {a: 7, c: 6, h: 1, l: 2, m: 2, q: 4, s: 4, t: 2, v: 1, z: 0}
+
+/**
+ * segment pattern
+ * @type {RegExp}
+ */
+
+var segment = /([astvzqmhlc])([^astvzqmhlc]*)/ig
+
+/**
+ * parse an svg path data string. Generates an Array
+ * of commands where each command is an Array of the
+ * form `[command, arg1, arg2, ...]`
+ *
+ * @param {String} path
+ * @return {Array}
+ */
+
+function parse(path) {
+	var data = []
+	path.replace(segment, function(_, command, args){
+		var type = command.toLowerCase()
+		args = parseValues(args)
+
+		// overloaded moveTo
+		if (type == 'm' && args.length > 2) {
+			data.push([command].concat(args.splice(0, 2)))
+			type = 'l'
+			command = command == 'm' ? 'l' : 'L'
+		}
+
+		while (true) {
+			if (args.length == length[type]) {
+				args.unshift(command)
+				return data.push(args)
+			}
+			if (args.length < length[type]) throw new Error('malformed path data')
+			data.push([command].concat(args.splice(0, length[type])))
+		}
+	})
+	return data
+}
+
+function parseValues(args){
+	args = args.match(/-?[.0-9]+(?:e[-+]?\d+)?/ig)
+	return args ? args.map(Number) : []
+}
+
+function join(parsed) {
+  var path = ""
+  parsed.forEach(function(p) {
+    var command = p.shift()
+    var nums = p.join(",")
+    path = path.concat(command + nums)
+  });
+  return path;
+}
