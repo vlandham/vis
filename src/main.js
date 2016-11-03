9da3a6ce2dadc4d59baf3858ca9840f9d45cda1f
Jim Vallandingham <vlandham@gmail.com>
2016-11-03 10:10:28 -0700

    more seasons

--- src/main.js
+++ src/main.js
@@ -13,10 +13,30 @@ function plotData(selector, data, plot) {
     .call(plot);
 }
 
+function cleanData(rawData) {
+  rawData.forEach(function(datum) {
+    datum.values.forEach(function(value) {
+      var dateValues = value.time.split('-');
+      var currentYear = new Date().getFullYear();
+      value.date = new Date(currentYear, (+dateValues[0] - 1), +dateValues[1])
+    });
+  });
+  return rawData;
+}
+
 function display(error, data) {
-  plotData("#vis",  data, plot);
+  data =  cleanData(data);
+  d3.select('#vis')
+    .selectAll('.chart')
+    .data(data)
+    .enter()
+    .append('div')
+    .classed('chart', true)
+    .each(function(d, i) {
+      plotData(this, d, plot);
+    });
 }
 
 queue()
-  .defer(d3.csv, "data/test.csv")
+  .defer(d3.json, "data/seasons.json")
   .await(display);
--- src/vis.js
+++ src/vis.js
@@ -3,15 +3,40 @@ var d3 = require('d3');
 
 module.exports = function createChart() {
   var width = 500;
-  var height = 500;
-  var margin = {top: 20, right: 20, bottom: 20, left: 20};
+  var height = 100;
+  var margin = {top: 20, right: 20, bottom: 25, left: 20};
   var g = null;
   var data = [];
 
+  var colors = d3.scale.category20();
+
+  var yScale = d3.scale.linear()
+    .domain([0.0, 1.0])
+    .range([height, 0]);
+
+  var currentYear = new Date().getFullYear();
+
+  var dateMin = new Date(currentYear, 0, 1);
+  var dateMax = new Date(currentYear, 11, 1);
+
+  var xScale = d3.time.scale()
+    .domain([dateMin, dateMax])
+    .range([0, width])
+
+  var xAxis = d3.svg.axis()
+    .scale(xScale)
+    .orient("bottom")
+    .tickFormat(d3.time.format("%b"));
+
+  var area = d3.svg.area()
+    .x(function(d) { return xScale(d.date); })
+    .y0(height)
+    .y1(function(d) { return yScale(d.amount); })
+    .interpolate("basis");
+
+
   var chart = function(selection) {
     selection.each(function(rawData) {
-
-      console.log(rawData);
 
       data = rawData;
 
@@ -22,24 +47,26 @@ module.exports = function createChart() {
       svg.attr("height", height + margin.top + margin.bottom );
       g = svg.select("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-      update();
+
+      g.append('text')
+        .classed('title', true)
+        .attr('font-size', '20px')
+        .text(function(d) { return d.name; });
+
+      g.append("path")
+        .datum(data)
+        .attr("class", "area")
+        .attr("fill", function(d) { return (d.color) ? d.color : colors(d.name)})
+        .attr("d", function(d) { return area(d.values); } );
+
+      g.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
+
     });
   };
 
-  function update() {
-    g.selectAll(".rect")
-      .data(data)
-      .enter()
-      .append("rect")
-      .attr("class", "rect")
-      .attr("fill", "steelblue")
-      .attr("x", d => d.x * 10)
-      .attr("y", d => d.y * 10)
-      .attr("width", 10)
-      .attr("height", 10)
-      .on("mouseover", function(d) { d3.select(this).attr("fill", "orange"); })
-      .on("mouseout", function(d) { d3.select(this).attr("fill", "steelblue"); });
-  }
 
   return chart;
 };
