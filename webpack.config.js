9057e401e15efde1e1feaa621f0f130af58d7736
Jim Vallandingham <vlandham@gmail.com>
2017-06-23 12:04:22 -0700

    use build folder

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ var minimize = process.argv.indexOf('--minimize') === -1 ? false : true;
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
 var plugins = [];
 
-plugins.push(new ExtractTextPlugin("app.css"));
+plugins.push(new ExtractTextPlugin("build/app.css"));
 
 if(minimize) {
   plugins.push(new webpack.optimize.UglifyJsPlugin());
@@ -17,7 +17,7 @@ module.exports = {
     javascript: './src/main.js'
   },
   output: {
-    filename: 'app.js'
+    filename: 'build/app.js'
   },
   debug: true,
   devtool: 'source-map',
@@ -40,12 +40,12 @@ module.exports = {
       {
         test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
         exclude: /(node_modules|bower_components)/,
-        loader: "file?name=[name].[ext]"
+        loader: "file?name=build/[name].[ext]"
         // loader: 'file-loader'
       },
       {
         test: /\.html$/,
-        loader: "file?name=[name].[ext]"
+        loader: "file?name=build/[name].[ext]"
       },
       {
         test: /\.scss$/,
